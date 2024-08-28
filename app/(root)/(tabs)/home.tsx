5914a14d89694882b2de204d3f29d6a797823448
Adnan <adbak.dev@gmail.com>
2024-08-28 23:58:26 +0200

    card

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,9 +1,8 @@
 import RideCard from "@/components/RideCard";
 import { rides } from "@/mock/rides";
-import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
-import { FlatList } from "react-native-reanimated/lib/typescript/Animated";
+import { useUser } from "@clerk/clerk-expo";
+import { FlatList } from "react-native";
+
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Page() {
